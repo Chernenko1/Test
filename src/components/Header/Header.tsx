d43121954562135d7d80c77591c9dad196caa766
Chertenko <kchek816@gmail.com>
2024-03-26 14:28:10 +0300

    fix: Header styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,28 +1,31 @@
 import './Header.css'
+import { IoSearch } from "react-icons/io5";
 
 export const Header = () => {
     return (
         <article className='main'>
-            <header>
-                <h1>Search for books</h1>
-                <div>
-                    <input type='search'/>
-                    <button title='поиск' type='submit'/>
-                </div>
-                <div>
+            <header className='header'>
+                <h1 className='headerTitle'>Search for books</h1>
+
+                    <div>
+                        <input type='search'/>
+                        <button title='поиск' type='submit'><IoSearch /></button>
+                    </div>
+
                     {/*Вынести  в отдельный компонент */}
-                    <div>
-                        <p>Categories</p>
-                        <select name='cat'/>
+                    <div className='selectionWrapper'>
+                        <div className='selectionContainer'>
+                            <p className='selectionTitle'>Categories</p>
+                            <select name='cat'/>
+                        </div>
+                        <div className='selectionContainer'>
+                            <p className='selectionTitle'>Sorting by</p>
+                            <select name='sort'>
+                                <option>1</option>
+                                <option>2</option>
+                            </select>
+                        </div>
                     </div>
-                    <div>
-                        <p>Sorting by</p>
-                        <select name='sort'>
-                            <option>1</option>
-                            <option>2</option>
-                        </select>
-                    </div>
-                </div>
             </header>
         </article>
     )
