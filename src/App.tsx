4dcc3801031766779e7d01343a9d5e3db8289fa9
Chertenko <kchek816@gmail.com>
2024-03-26 20:17:23 +0300

    -

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,31 @@
 import { useState } from 'react'
-
-import './App.css'
 import { Header } from './components/Header/Header'
 import { BooksList } from './components/BookComponents/BooksList'
+import { useGetBooksQuery } from './redux/services/books'
+import { useAppSelector } from './redux/hooks'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [category, setCategory] = useState('')
+  const [order, setOrder] = useState('relevance')
+  const [input, setInput] = useState('')
+
+  const {data, isLoading, isError,isSuccess,error} = useGetBooksQuery({name: input, order, category})
+
+  const books = useAppSelector(state => state.books.books)
+
+
+  // if(isLoading) {
+  //   console.log('load')
+  // } else if(isError) {
+  //   console.log(error)
+  // } else if(isSuccess) {
+  //   console.log(data)
+  // }
 
   return (
     <>
-      <Header />
-      <BooksList />
+      <Header setInput={setInput} setCategory={setCategory} setOrder={setOrder}/>
+      <BooksList books={books}/>
     </>
   )
 }
