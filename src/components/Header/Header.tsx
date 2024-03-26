73d00388648335b275b0ee3af4f6780b7347ecfc
Chertenko <kchek816@gmail.com>
2024-03-26 20:16:48 +0300

    feat: added func

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,31 +1,63 @@
-import './Header.css'
+import React, { ChangeEvent } from 'react';
+import style from './Header.module.css'
 import { IoSearch } from "react-icons/io5";
 
-export const Header = () => {
+interface IHeader {
+    setCategory: (val: string) => void,
+    setOrder: (val: string) => void,
+    setInput: (val: string) => void
+}
+
+export const Header = ({setCategory, setInput, setOrder}: IHeader) => {
+
+    function handleSubmit () {
+       
+    }
+
+    function changeInput (event: React.ChangeEvent<HTMLInputElement>){
+        setInput(event.target.value)
+    }
+
+    function selectCategory (event: ChangeEvent<HTMLSelectElement>) {
+        setCategory(event.target.value)
+    }
+
+    function selectOrder (order: string) {
+        setOrder(order)
+    }
+
     return (
-        <article className='main'>
-            <header className='header'>
-                <h1 className='headerTitle'>Search for books</h1>
-
-                    <div>
-                        <input type='search'/>
-                        <button title='поиск' type='submit'><IoSearch /></button>
-                    </div>
-
-                    {/*Вынести  в отдельный компонент */}
-                    <div className='selectionWrapper'>
-                        <div className='selectionContainer'>
-                            <p className='selectionTitle'>Categories</p>
-                            <select name='cat'/>
+        <article className={style.main}>
+            <header className={style.header}>
+                <h1 className={style.headerTitle}>Search for books</h1>
+                    <label>
+                        <input name='search' type='search' onChange={(e) => changeInput(e)} />
+                        <button title='поиск' type='submit' onClick={handleSubmit}><IoSearch /></button>
+                    </label>
+                    
+                    <div className={style.selectionWrapper}>
+                        <div className={style.selectionContainer}>
+                            <p className={style.selectionTitle}>Categories</p>
+                            <select defaultValue='all' onChange={(e) => selectCategory(e)}>
+                                <option>all</option>
+                                <option>art</option>
+                                <option>biography</option>
+                                <option>computers</option>
+                                <option>history</option>
+                                <option>medical</option>
+                                <option>poetry</option>
+                            </select>
                         </div>
-                        <div className='selectionContainer'>
-                            <p className='selectionTitle'>Sorting by</p>
-                            <select name='sort'>
-                                <option>1</option>
-                                <option>2</option>
+                        <div className={style.selectionContainer}>
+                            <p className={style.selectionTitle}>Sorting by</p>
+                            <select defaultValue='relevance' onChange={(e) => selectOrder(e.target.value)}>
+                                <option value='relevance'>relevance</option>
+                                <option value='newest'>newest</option>
                             </select>
                         </div>
                     </div>
+
+                    {/*Вынести  в отдельный компонент */}
             </header>
         </article>
     )
