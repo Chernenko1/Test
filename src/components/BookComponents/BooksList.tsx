5f072a2881b1c86a3a42eb8ccf040f0fa8db8c41
Chertenko <kchek816@gmail.com>
2024-03-27 16:20:22 +0300

    feat: loadMore button

--- src/components/BookComponents/BooksList.tsx
+++ src/components/BookComponents/BooksList.tsx
@@ -1,22 +1,39 @@
 import { useAppSelector } from '../../redux/hooks'
+import { LoadSpinner } from '../LoadSpinner/LoadSpinner'
 import { BookCard } from './BookCard'
 import style from './styles/BooksList.module.css'
 
-export const BooksList = () => {
+interface IBookList {
+  isLoading: boolean
+  load: () => void
+}
+
+export const BooksList = ({ isLoading, load }: IBookList) => {
   const books = useAppSelector((state) => state.books.books)
 
-  return (
-    <article className={style.main}>
-      {books.map((book, ind) => (
-        <BookCard
-          key={ind}
-          category={book.categories[0]}
-          bookName={book.bookName}
-          authors={book.authors}
-          id={book.id}
-          img={book.image}
-        />
-      ))}
+  return isLoading ? (
+    <LoadSpinner />
+  ) : (
+    <article>
+      <section className={style.main}>
+        {books.map((book, ind) => (
+          <BookCard
+            key={ind}
+            category={book.categories[0]}
+            bookName={book.bookName}
+            authors={book.authors}
+            id={book.id}
+            img={book.image}
+          />
+        ))}
+      </section>
+      {books.length && (
+        <section className={style.buttonContainer}>
+          <button className={style.button} onClick={load}>
+            Load More
+          </button>
+        </section>
+      )}
     </article>
   )
 }
