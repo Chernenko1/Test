57a59ace51fa57794dce27fcac98361f8501b1e7
Chertenko <kchek816@gmail.com>
2024-03-27 11:53:32 +0300

    fix: url brought in env

--- src/redux/services/books.ts
+++ src/redux/services/books.ts
@@ -1,41 +1,47 @@
-import { createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
-import { setBooks } from "../slices/booksSlice";
-
-// console.log(process.env.VITE_API_KEY)
-
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { setBooks } from '../slices/booksSlice'
 
 export const booksApi = createApi({
-    reducerPath: 'bookApi',
-    baseQuery: fetchBaseQuery({baseUrl: 'https://www.googleapis.com/books/v1/volumes',}),
-    endpoints: builder => ({
-        getBooks: builder.query({
-            query: ({name, category, sort}) => `?q=intitle:${name}+subject:${category}&maxResults=30&orderBy=${sort}&key=${import.meta.env.VITE_API_KEY}`,
-            async onQueryStarted ({}, {queryFulfilled, dispatch}) {
-                try {
-                    let booksArr = []
-                    const {data} = await queryFulfilled
-                    if(data.totalItems !== 0) {
-                    data.items.map((item: {id: string, volumeInfo: {title: string, authors: string[], categories: string[], imageLinks: {smallThumbnail: string}}}) => 
-                    {
-                        booksArr.push({
-                            id: item.id, 
-                            bookName: item.volumeInfo.title, 
-                            authors: item.volumeInfo.authors ?? [''],
-                            categories: item.volumeInfo.categories ?? [''],
-                            image: item.volumeInfo.imageLinks.smallThumbnail
-                        })
-                    }) 
-                    dispatch(setBooks(booksArr))
-                } else {
-
-                    dispatch(setBooks([]))
+  reducerPath: 'bookApi',
+  baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_BASE_URL }),
+  endpoints: (builder) => ({
+    getBooks: builder.query({
+      query: ({ name, category, sort }) =>
+        `?q=intitle:${name}+subject:${category}&maxResults=30&orderBy=${sort}&key=${import.meta.env.VITE_API_KEY}`,
+      async onQueryStarted({}, { queryFulfilled, dispatch }) {
+        try {
+          let booksArr = []
+          const { data } = await queryFulfilled
+          if (data.totalItems !== 0) {
+            data.items.map(
+              (item: {
+                id: string
+                volumeInfo: {
+                  title: string
+                  authors: string[]
+                  categories: string[]
+                  imageLinks: { smallThumbnail: string }
                 }
-                } catch (error) {
-                    console.log(error)
-                }
-            }
-        })
-    })
+              }) => {
+                booksArr.push({
+                  id: item.id,
+                  bookName: item.volumeInfo.title,
+                  authors: item.volumeInfo.authors ?? [''],
+                  categories: item.volumeInfo.categories ?? [''],
+                  image: item.volumeInfo.imageLinks.smallThumbnail,
+                })
+              },
+            )
+            dispatch(setBooks(booksArr))
+          } else {
+            dispatch(setBooks([]))
+          }
+        } catch (error) {
+          console.log(error)
+        }
+      },
+    }),
+  }),
 })
 
-export const {useGetBooksQuery} = booksApi
+export const { useGetBooksQuery } = booksApi
