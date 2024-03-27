b2f04194cefc718a3e05881b61f98083f23603e7
Chertenko <kchek816@gmail.com>
2024-03-27 11:49:20 +0300

    fix: rewrite on SectionList

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,65 +1,54 @@
-import React, { ChangeEvent } from 'react';
+import React, { ChangeEvent } from 'react'
 import style from './Header.module.css'
-import { IoSearch } from "react-icons/io5";
+import { IoSearch } from 'react-icons/io5'
+import { SectionList } from '../SectionList/SectionList'
+import { BOOK_CATEGORIES } from '../../constants/BOOK_CATEGORIES'
+import { SORT_TYPES } from '../../constants/SORT_TYPES'
 
 interface IHeader {
-    setCategory: (val: string) => void,
-    setOrder: (val: string) => void,
-    setInput: (val: string) => void
-    search: () => void
+  setCategory: (val: string) => void
+  setOrder: (val: string) => void
+  setInput: (val: string) => void
+  search: () => void
 }
 
-export const Header = ({setCategory, setInput, setOrder,search}: IHeader) => {
+export const Header = ({ setCategory, setInput, setOrder, search }: IHeader) => {
+  function changeInput(event: React.ChangeEvent<HTMLInputElement>) {
+    setInput(event.target.value)
+  }
 
-    function handleSubmit () {
-       
-    }
+  function changeCategory(event: ChangeEvent<HTMLSelectElement>) {
+    setCategory(event.target.value)
+  }
 
-    function changeInput (event: React.ChangeEvent<HTMLInputElement>){
-        setInput(event.target.value)
-    }
+  function changeOrderBy(event: ChangeEvent<HTMLSelectElement>) {
+    setOrder(event.target.value)
+  }
 
-    function selectCategory (event: ChangeEvent<HTMLSelectElement>) {
-        setCategory(event.target.value)
-    }
+  return (
+    <article className={style.main}>
+      <header className={style.header}>
+        <h1 className={style.headerTitle}>Search for books</h1>
+        <label>
+          <input name='search' type='search' onChange={changeInput} />
+          <button title='поиск' type='submit' onClick={search}>
+            <IoSearch />
+          </button>
+        </label>
 
-    function selectOrder (order: string) {
-        setOrder(order)
-    }
+        <div className={style.selectionWrapper}>
+          <div className={style.selectionContainer}>
+            <p className={style.selectionTitle}>Categories</p>
+            <SectionList option={BOOK_CATEGORIES} onChange={changeCategory} />
+          </div>
+          <div className={style.selectionContainer}>
+            <p className={style.selectionTitle}>Sorting by</p>
+            <SectionList option={SORT_TYPES} onChange={changeOrderBy} />
+          </div>
+        </div>
 
-    return (
-        <article className={style.main}>
-            <header className={style.header}>
-                <h1 className={style.headerTitle}>Search for books</h1>
-                    <label>
-                        <input name='search' type='search' onChange={(e) => changeInput(e)} />
-                        <button title='поиск' type='submit' onClick={search}><IoSearch /></button>
-                    </label>
-                    
-                    <div className={style.selectionWrapper}>
-                        <div className={style.selectionContainer}>
-                            <p className={style.selectionTitle}>Categories</p>
-                            <select defaultValue='all' onChange={(e) => selectCategory(e)}>
-                                <option>all</option>
-                                <option>art</option>
-                                <option>biography</option>
-                                <option>computers</option>
-                                <option>history</option>
-                                <option>medical</option>
-                                <option>poetry</option>
-                            </select>
-                        </div>
-                        <div className={style.selectionContainer}>
-                            <p className={style.selectionTitle}>Sorting by</p>
-                            <select defaultValue='relevance' onChange={(e) => selectOrder(e.target.value)}>
-                                <option value='relevance'>relevance</option>
-                                <option value='newest'>newest</option>
-                            </select>
-                        </div>
-                    </div>
-
-                    {/*Вынести  в отдельный компонент */}
-            </header>
-        </article>
-    )
+        {/*Вынести  в отдельный компонент */}
+      </header>
+    </article>
+  )
 }
