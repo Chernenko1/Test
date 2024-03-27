447f712d0139f536ac95bc89811a7b4ba0290366
Chertenko <kchek816@gmail.com>
2024-03-27 11:24:45 +0300

    fix: deleted http fd

--- src/components/http/booksAPI.ts
+++
@@ -1,6 +0,0 @@
-import axios from "axios"
-
-export const getBooks = async (name: string,sort: string, category: string) => {
-    const {data} = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=intitle:${name}+subject:${category}&maxResults=30&orderBy${sort}&key=AIzaSyBq5vlNepCzq44T-ohOge8ilN3q6d3H7eU`)
-    return data
-}
